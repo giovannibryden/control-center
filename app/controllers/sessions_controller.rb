9c3dddb24b3fcd626a544a504ce80f5f28e0fba8
Giovanni <giovannibryden@gmail.com>
2017-04-11 16:13:47 -0400

    Added a lot of things.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,32 @@
+class SessionsController < ApplicationController
+
+include SessionsHelper
+  
+  def new
+  end
+
+  def create
+  	user = User.find_by_email(params[:session][:email].downcase)
+
+  	# Check to see if user exists AND that they can authenticate with the password
+  	if user && user.authenticate(params[:session][:password])
+  		flash[:success] = "Whoa. It worked!"
+  		log_in user
+      remember user
+      render 'success'
+  	else
+  		flash[:notice] = "Something's wrong!"
+      render 'new'
+  	end
+  end	
+
+  def success
+    @email = params[:session][:email]
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to root_path
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-	resources :users
-	get  '/signup',  to: 'users#new'
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  # root 'topics#index'
+  get 'signup' => 'users#new'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
+  
 end
