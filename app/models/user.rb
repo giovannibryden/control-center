9ec36386b173c0cdc3a20ff9ce4a023f7f9c6f9c
Giovanni <giovannibryden@gmail.com>
2017-04-11 14:04:45 -0400

    Add register form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	validates :email, presence: true, length: {maximum:255}, format: {with:VALID_EMAIL_REGEX}, uniqueness: {case_sensitive:false}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: {maximum:255}, format: {with:VALID_EMAIL_REGEX}, uniqueness: {case_sensitive:false}
 
 end
