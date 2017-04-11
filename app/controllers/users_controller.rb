2fa679fb4017bcf0a9fc804f8899e7ca53d73508
Giovanni <giovannibryden@gmail.com>
2017-04-11 14:55:59 -0400

    Changes to user controller, model, and signup page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,26 @@ class UsersController < ApplicationController
   def new
   	@user = User.new
   end
-
+  
   def create
-  	
+  	@user = User.new(user_params)
+  	if @user.save
+  		flash[:notice] = 'Successfully created an account!'
+  		log_in @user
+      render 'success'
+  	else
+  		flash[:notice] = 'Oops!'
+  		render 'new'
+    end
   end
+
+  def success
+    @user = User.find_by(params[:id])
+  end
+
+private
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   resources :users
+  
+  get  '/signup',  to: 'users#new'
 end
