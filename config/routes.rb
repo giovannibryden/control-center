176d65a8c24e12aa4da6c8cb9ccc705e515dbca9
Giovanni <giovannibryden@gmail.com>
2017-04-11 16:48:12 -0400

    Added log in, and log out functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'static/home'
+
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
-  # root 'topics#index'
+  root 'static#home'
   get 'signup' => 'users#new'
 
   get 'login' => 'sessions#new'
