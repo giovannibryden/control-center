6c40dfcf33973e83390a15e30c4a1db02ae0b6ce
Giovanni Bryden <giovannibryden@GiovanniBryden-MacBookPro.local>
2017-11-29 11:46:40 -0500

    Whoops

--- db/migrate/20171129160311_add_verified_to_feeds.rb
+++ db/migrate/20171129160311_add_verified_to_feeds.rb
@@ -1,5 +1,5 @@
 class AddVerifiedToFeeds < ActiveRecord::Migration[5.0]
   def change
-  	add_column :feeds, :verified, :string, default: "False"
+  	add_column :feeds, :verified, :string, :default => "False"
   end
 end
