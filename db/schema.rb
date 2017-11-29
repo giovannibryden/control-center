7d042b0181c2401e88660ee8a9093fee3a5f15ff
Giovanni Bryden <giovannibryden@GiovanniBryden-MacBookPro.local>
2017-11-29 11:07:25 -0500

    Add verified field to Feeds table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128201003) do
+ActiveRecord::Schema.define(version: 20171129160311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20171128201003) do
     t.string   "api_secret"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "verified"
   end
 
   create_table "projects", force: :cascade do |t|
