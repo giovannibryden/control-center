1bf604787ed7fbcb5084584e6a5cbf3c7ff18e48
Giovanni Bryden <giovannibryden@GiovanniBryden-MacBookPro.local>
2017-11-29 11:44:36 -0500

    Verified defaults to false once created.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171129160311) do
+ActiveRecord::Schema.define(version: 20171129164001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,9 +19,10 @@ ActiveRecord::Schema.define(version: 20171129160311) do
     t.string   "source"
     t.string   "api_key"
     t.string   "api_secret"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.string   "verified"
+    t.datetime "verified_date"
   end
 
   create_table "projects", force: :cascade do |t|
